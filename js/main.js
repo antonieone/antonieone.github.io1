0fb18df664c8055072f254514000148ead92a8c9
Anton Anton <antonieone@gmail.com>
2020-07-20 10:29:34 +0300

    отредоктирован код сайта

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-// let productPage = document.querySelector('.product_page')
 let productPage = document.querySelector('.nav')
-let hamburgerMenu = document.querySelector('.hamburger_menu')
+let hamburgerMenu = document.querySelector('.nav__icons')
 
 hamburgerMenu.addEventListener('click', function () {
   productPage.classList.toggle('menu_open')
---
+++ js/map.js
@@ -0,0 +1,10 @@
+
+function initMap() {
+  var uluru = {lat: -51.503324, lng: 0.1217317};
+  var map = new google.maps.Map(document.getElementById('map'),
+      {
+        zoom: 4,
+        center: uluru
+      });
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
