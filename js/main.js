356d1b732a49e491e720b09e9d7408c29f8d7d0f
Anton Anton <antonieone@gmail.com>
2020-08-10 15:49:56 +0300

    Добавлен slick-slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,53 @@ let hamburgerMenu = document.querySelector('.nav__icons')
 hamburgerMenu.addEventListener('click', function () {
   productPage.classList.toggle('menu_open')
 })
+
+
+$(document).ready(function(){
+  $('.slider').slick({
+    speed: 1500,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+    ]
+  });
+});
+
+
+$(document).ready(function(){
+  $('.slider-disc').slick({
+    speed: 1500,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+    ]
+  });
+});
